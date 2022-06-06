3dd74cefb04295b38c9ba8e324c41964c05f8af7
Jay Prakash Sharma <jayprakashsharma7@icloud.com>
2022-06-06 22:11:38 +0530

    Bug fixed

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -6,21 +6,8 @@
 //
 
 import UIKit
-import Foundation
-
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate,UITextFieldDelegate{
+class ViewController: UIViewController{
     
-// View Data in tableView
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.name.animalName.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = self.tableView.dequeueReusableCell(withIdentifier: "animalCell", for: indexPath) as UITableViewCell
-        cell.textLabel?.text = self.name.animalName[indexPath.row]
-        return cell
-    }
-
     // Variable
     var name:Animal = Animal(name: [])
 
@@ -38,17 +25,18 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         tableView.delegate = self
         tableView.dataSource = self
         dataTextField.delegate = self
+        dataTextField.becomeFirstResponder()
     }
 
 // Add Data
     @IBAction func addData(_ sender: UIButton) {
         if sender.tag == 1 && dataTextField.text != ""{
         name.animalName.append(dataTextField.text!)
+        dataTextField.resignFirstResponder()
         dataTextField.text = ""
         let alert = UIAlertController(title: "Message", message: "Data Added Successfully", preferredStyle: UIAlertController.Style.alert)
                 alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
                 self.present(alert, animated: true, completion: nil)
-        print(name.animalName)
         self.tableView.reloadData()
         }else{
             let alert = UIAlertController(title: "Message", message: "Please enter animal name", preferredStyle: UIAlertController.Style.alert)
@@ -56,7 +44,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                 self.present(alert, animated: true, completion: nil)
                 self.tableView.reloadData()
         }
-}
+    }
     
 // Delete Data
     @IBAction func deleteData(_ sender: UIButton) {
@@ -65,7 +53,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let alert = UIAlertController(title: "Message", message: "Data Deleted Successfully", preferredStyle: UIAlertController.Style.alert)
             alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
             self.present(alert, animated: true, completion: nil)
-        print(name.animalName)
         self.tableView.reloadData()
         }else{
             let alert = UIAlertController(title: "Message", message: "No Data Found", preferredStyle: UIAlertController.Style.alert)
@@ -74,4 +61,37 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                 self.tableView.reloadData()
         }
     }
+}
+
+// View Data in tableViewDelegate, tableViewDataSource and UITextFieldDelegate
+    extension ViewController:UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.name.animalName.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "animalCell", for: indexPath) as UITableViewCell
+        cell.textLabel?.text = self.name.animalName[indexPath.row]
+        return cell
+    }
+        
+// Return Key
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            if dataTextField.text != "" {
+            name.animalName.append(dataTextField.text!)
+            dataTextField.text = ""
+            dataTextField.resignFirstResponder()
+            let alert = UIAlertController(title: "Message", message: "Data Added Successfully", preferredStyle: UIAlertController.Style.alert)
+                    alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+            self.tableView.reloadData()
+                print(name.animalName)
+                }else{
+                    let alert = UIAlertController(title: "Message", message: "Please enter animal name", preferredStyle: UIAlertController.Style.alert)
+                        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+                        self.present(alert, animated: true, completion: nil)
+                        self.tableView.reloadData()
+                }
+            return true
+        }
 }
